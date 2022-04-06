8d80f62119a597da575bec0a1a6d5707522f9a43
Dhaesha M <dhaesha1@gmail.com>
2022-04-06 11:09:51 -0400

    setting up UI w Navbar component that can utilize useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
+import MainMint from "./MainMint";
+import NavBar from "./Navbar";
 
 function App() {
-  return <div className="App">Dappin Good!</div>;
+  const [accounts, setAccounts] = useState([]);
+
+  return (
+    <div className="App">
+      <NavBar accounts={accounts} setAccounts={setAccounts} />
+    </div>
+  );
 }
 
 export default App;
